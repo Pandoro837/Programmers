63ea6e7d2913e56300186f5d77baea9633a44a0f
최영교 <mould837@naver.com>
2021-12-08 23:16:40 +0900

    1208

--- src/com/level/one/FindPrimeNum.java
+++ src/com/level/one/FindPrimeNum.java
@@ -4,7 +4,24 @@ public class FindPrimeNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 1574;
+		int n2 = 124;
+		
+		System.out.println(solution(n));
+		System.out.println(solution(n2));
+		
 	}
 
+	private static int solution(int n) {
+		int answer = 0;
+		
+		for(int i = 2; i < n; i++) {
+			for(int j = 2; j < i/2; j++) {
+				
+			}
+		}
+		
+		return answer;
+	}
+	
 }
